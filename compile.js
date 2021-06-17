1adcebea0c4f9a3db85a2d3dd413d60c7e22d3e3
JustinRyukiRogers <jr.rogers@mail.utoronto.ca>
2021-06-17 14:17:24 -0400

    Initial Commit

---
+++ compile.js
@@ -0,0 +1,10 @@
+// can't call .sol file cuz this is .js
+const path = require("path");
+const fs = require("fs");
+const solc = require("solc");
+
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
+const source = fs.readFileSync(inboxPath, "utf8");
+
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
+// need to make it available
---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+const provider = new HDWalletProvider(
+  //'12 word mneumonic here',
+  'https://rinkeby.infura.io/v3/528bdb81c61549229fe315ac52d95719'
+);
+
+const web3 = new Web3(provider);
+
+// similar to the test
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account',accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode, arguments: ['Hi there!']})
+    .send({gas: '1000000', from: accounts[0]});
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
